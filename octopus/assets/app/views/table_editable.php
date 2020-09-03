c95b4d121e2e259d8ed188b5b5b5b8527a9981e7
Luis-Salinas-SD <paus.impala21@gmail.com>
2020-09-03 17:25:12 -0500

    Actualizacion del archivo alerts.php

---
+++ octopus/assets/app/views/table_editable.php
@@ -0,0 +1,32 @@
+
+
+<!doctype html>
+<html class="fixed">
+   <?php include("../../includes/head.php");?>
+    <body>
+        <section class="body">
+
+            <!-- start: header -->
+          <?php include("../../includes/header.php");?>
+            <!-- end: header -->
+
+            <div class="inner-wrapper">
+                <!-- start: sidebar -->
+              <?php include("../../includes/sidebar.php");?>
+                <!-- end: sidebar -->
+                
+                <!-- inicio principal de blank page -->               
+               <?php include("../../includes/blank_page_principal.php");?> 
+                 <!-- final principal de blank page -->
+                </div>
+                
+            <!-- inicio principal de blank page -->               
+               <?php include("../../includes/sidebar_blank_page.php");?> 
+                 <!-- final principal de blank page -->
+            
+        </section>
+
+        <?php include("../../includes/scripts.php"); ?>
+
+    </body>
+</html>
